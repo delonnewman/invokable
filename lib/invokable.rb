41ef237f8c6b86d5b4703049f04a79d9a5fbaaf7
Delon Newman <contact@delonnewman.name>
2020-02-10 09:56:19 -0700

    Updated documentation
    - Updated README.md
    - Added inline documentation for Invokable#curry

--- lib/invokable.rb
+++ lib/invokable.rb
@@ -1,9 +1,10 @@
 require 'invokable/version'
 
-# TODO: Add curry, memoize, transducers?
+# TODO: Add memoize, transducers?
 module Invokable
   # If object responds to `call` convert into a Proc forwards it's arguments along to `call`.
   #
+  # @see [Proc#call]
   # @return [Proc]
   def to_proc
     if respond_to?(:call)
@@ -16,7 +17,14 @@ module Invokable
     end
   end
 
-  def curry
-    to_proc.curry
+  # Return a curried proc. If the optional arity argument is given, it determines the number of arguments.
+  # A curried proc receives some arguments. If a sufficient number of arguments are supplied, it passes the
+  # supplied arguments to the original proc and returns the result. Otherwise, returns another curried proc
+  # that takes the rest of arguments.
+  #
+  # @see [Proc#curry]
+  # @return [Proc]
+  def curry(*args)
+    to_proc.curry(*args)
   end
 end
