264fe6c6587b3f6e827dd4b604dfa8147660f4d9
Delon Newman <contact@delonnewman.name>
2020-02-10 10:35:30 -0700

    Improved API documentation, Array#call is now mapped to Array#[]

--- lib/invokable/array.rb
+++ lib/invokable/array.rb
@@ -1,6 +1,10 @@
 require_relative '../invokable'
 
+# Extend core Array object by aliasing it's `[]` method as `call`,
+# and including the `Invokable` module.
+#
+# @see https://ruby-doc.org/core-2.7.0/Array.html#method-i-5B-5D Array#[]
 class Array
   include Invokable
-  alias call at
+  alias call []
 end
--- lib/invokable/hash.rb
+++ lib/invokable/hash.rb
@@ -1,5 +1,9 @@
 require_relative '../invokable'
 
+# Extend core Hash object by aliasing it's `dig` method as `call`,
+# and including the `Invokable` module.
+#
+# @see https://ruby-doc.org/core-2.7.0/Hash.html#method-i-dig Hash#dig
 class Hash
   include Invokable
   alias call dig
